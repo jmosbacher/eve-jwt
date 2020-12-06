db73ce28bbc65c1b278a6e9789f73df332473f4e
Yossi Mosbacher <joe.mosbacher>
2020-12-06 15:08:58 +0200

    resource_def optional

--- eve_jwt/auth.py
+++ eve_jwt/auth.py
@@ -108,7 +108,7 @@ class JWTAuth(TokenAuth):
         If the validation succeed, the claims are stored and accessible thru the
         get_authen_claims() method.
         """
-        resource_conf = config.DOMAIN[resource]
+        resource_conf = config.DOMAIN.get(resource, {})
         audiences = resource_conf.get('audiences', config.JWT_AUDIENCES)
         return self._perform_validation(token, audiences, allowed_roles)
 
